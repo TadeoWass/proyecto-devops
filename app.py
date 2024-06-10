d946fba164853729799c768764c8434c49a92827
TadeoWass <wassnertadeo@gmail.com>
2024-06-10 17:40:59 -0300

    New Relic

--- app.py
+++ app.py
@@ -5,6 +5,10 @@ from flask import Flask
 # Configuración de New Relic
 newrelic.agent.initialize('newrelic.ini')
 
+@newrelic.agent.background_task(name='database-update', group='Task')
+def database_update():
+    ...
+
 app = Flask(__name__)
 
 @app.route('/')
