20dde74b7f07fc13f3c694417861c0442a5c419b
TadeoWass <wassnertadeo@gmail.com>
2024-06-11 21:54:10 -0300

    CI V4

--- tests/test_app.py
+++ tests/test_app.py
@@ -10,5 +10,13 @@ class TestApp(unittest.TestCase):
         rv = self.app.get('/')
         self.assertEqual(rv.data.decode('utf-8'), 'Hola Mundo')
 
+    def test_error_500(self):
+        rv = self.app.get('/error500')
+        self.assertEqual(rv.status_code, 500)
+
+    def test_error_400(self):
+        rv = self.app.get('/error400')
+        self.assertEqual(rv.status_code, 400)
+
 if __name__ == "__main__":
     unittest.main()
